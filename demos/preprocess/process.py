a6a473c8e43f84478b109f17ec4d54932b701f6f
wwwjjjnnn <1194348056@qq.com>
2019-11-16 16:30:01 +0800

    update preprocess.

---
+++ demos/preprocess/process.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*
+import os
+from xml.dom.minidom import parse
+
+from configurations.constant import Constant
+
+TYPE = 'bert'
+
+
+class Process(object):
+    def __init__(self):
+        self.project_path = Constant(type=TYPE).get_project_path()
+        self.configuration = Constant(type=TYPE).get_configuration()
+
+        self.train_set_xml_data = os.path.join(self.project_path, self.configuration.train_set_path)
+        self.train_data_cache_path = os.path.join(self.project_path, self.configuration.train_data_cache_path)
+
+    @staticmethod
+    def generate_train_data_pair(equ_questions, not_equ_questions):
+        a = [x + "\t" + y + "\t" + "0" for x in equ_questions for y in not_equ_questions]
+        # 包含重复值,只是顺序不同 结果：100298条数据
+        # b = [x + "\t" + y + "\t" + "1" for x in equ_questions for y in equ_questions if x != y]
+        # 不包含重复值 结果：81080条数据
+        b = []
+        for s_index in range(len(equ_questions) - 1):
+            for e_index in range(s_index + 1, len(equ_questions)):
+                b.append(equ_questions[s_index] + "\t" + equ_questions[e_index] + "\t" + "1")
+
+        return a + b
+
+    @staticmethod
+    def write_train_data(file, pairs):
+        with open(file, mode="w") as f:
+            for pair in pairs:
+                f.write(pair + "\n")
+
+    def parse_train_data(self):
+        """
+        构造训练集
+        :return:
+        """
+        pair_list = []
+        doc = parse(self.train_set_xml_data)
+        collection = doc.documentElement
+        for i in collection.getElementsByTagName("Questions"):
+            # if i.hasAttribute("number"):
+            #     print ("Questions number=", i.getAttribute("number"))
+            EquivalenceQuestions = i.getElementsByTagName("EquivalenceQuestions")
+            NotEquivalenceQuestions = i.getElementsByTagName("NotEquivalenceQuestions")
+            equ_questions = EquivalenceQuestions[0].getElementsByTagName("question")
+            not_equ_questions = NotEquivalenceQuestions[0].getElementsByTagName("question")
+            equ_questions_list, not_equ_questions_list = [], []
+            for q in equ_questions:
+                try:
+                    equ_questions_list.append(q.childNodes[0].data.strip())
+                except:
+                    continue
+            for q in not_equ_questions:
+                try:
+                    not_equ_questions_list.append(q.childNodes[0].data.strip())
+                except:
+                    continue
+            pair = Process.generate_train_data_pair(equ_questions_list, not_equ_questions_list)
+            pair_list.extend(pair)
+        print("All pair count=", len(pair_list))
+
+        # 保存结果
+        Process.write_train_data(self.train_data_cache_path, pair_list)
+
+        return pair_list
+
+
+if __name__ == "__main__":
+    pair_list = Process().parse_train_data()
