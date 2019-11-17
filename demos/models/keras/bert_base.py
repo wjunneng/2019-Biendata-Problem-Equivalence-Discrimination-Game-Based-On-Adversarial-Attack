dba7a11f43a8cbae3377b30dc147dcfda6c24971
wwwjjjnnn <1194348056@qq.com>
2019-11-17 20:39:05 +0800

    update bert.

--- demos/models/keras/bert_base.py
+++ demos/models/keras/bert_base.py
@@ -4,188 +4,99 @@ import os
 "摘自https://zhuanlan.zhihu.com/p/82737301"
 import pandas as pd
 import numpy as np
-from tqdm import tqdm
-from sklearn.model_selection import StratifiedKFold
-from keras_bert import load_trained_model_from_checkpoint, Tokenizer
-from keras.optimizers import Adam
-from sklearn.metrics import mean_absolute_error, accuracy_score, f1_score
-from keras.layers import *
-from keras.models import Model
 import keras.backend as K
-from keras.callbacks import Callback
+from configurations.constant import Constant
 
-learning_rate = 5e-5
-min_learning_rate = 1e-5
+from demos.models.keras.util import DataGenerator, Evaluate
+from demos.models.keras.util import Util
 
-config_path = '/home/wjunneng/Ubuntu/2019-Biendata-Problem-Equivalence-Discrimination-Game-Based-On-Adversarial-Attack/chinese_L-12_H-768_A-12/bert_config.json'
-checkpoint_path = '/home/wjunneng/Ubuntu/2019-Biendata-Problem-Equivalence-Discrimination-Game-Based-On-Adversarial-Attack/chinese_L-12_H-768_A-12/bert_model.ckpt'
-dict_path = '/home/wjunneng/Ubuntu/2019-Biendata-Problem-Equivalence-Discrimination-Game-Based-On-Adversarial-Attack/chinese_L-12_H-768_A-12/vocab.txt'
-
-MAX_LEN = 64
-
-token_dict = {}
-with open(dict_path, 'r', encoding='utf-8') as reader:
-    for line in reader:
-        token = line.strip()
-        token_dict[token] = len(token_dict)
-tokenizer = Tokenizer(token_dict)
-
-train = pd.read_csv('/home/wjunneng/Ubuntu/2019-Biendata-Problem-Equivalence-Discrimination-Game-Based-On-Adversarial-Attack/datasources/cache/train_data.csv')
-test = pd.read_csv('/home/wjunneng/Ubuntu/2019-Biendata-Problem-Equivalence-Discrimination-Game-Based-On-Adversarial-Attack/datasources/original/dev_set.csv', sep='\t')
-train_achievements = train['question1'].values
-train_requirements = train['question2'].values
-labels = train['label'].values
+TYPE = 'bert'
 
 
-def label_process(x):
-    if x == 0:
-        return [1, 0]
-    else:
-        return [0, 1]
+class BertBase(object):
+    def __init__(self):
+        self.configuration = Constant(type=TYPE).get_configuration()
+        self.project_path = Constant(type=TYPE).get_project_path()
 
+        self.train_set_path = os.path.join(self.project_path, self.configuration.train_data_csv_cache_path)
+        self.dev_set_path = os.path.join(self.project_path, self.configuration.dev_set_path)
 
-train['label'] = train['label'].apply(label_process)
-labels_cat = list(train['label'].values)
-labels_cat = np.array(labels_cat)
-test_achievements = test['question1'].values
-test_requirements = test['question2'].values
-print(train.shape, test.shape)
+        self.bert_config_path = os.path.join(self.project_path, self.configuration.bert_config_path)
+        self.bert_checkpoint_path = os.path.join(self.project_path, self.configuration.bert_checkpoint_path)
+        self.bert_vocab_path = os.path.join(self.project_path, self.configuration.bert_vocab_path)
 
+        self.MAX_LEN = self.configuration.MAX_LEN
 
-class data_generator:
-    def __init__(self, data, batch_size=16):
-        self.data = data
-        self.batch_size = batch_size
-        self.steps = len(self.data[0]) // self.batch_size
-        if len(self.data[0]) % self.batch_size != 0:
-            self.steps += 1
+        self.learning_rate = self.configuration.learning_rate
+        self.min_learning_rate = self.configuration.learning_rate
 
-    def __len__(self):
-        return self.steps
-
-    def __iter__(self):
-        while True:
-            X1, X2, y = self.data
-            idxs = list(range(len(self.data[0])))
-            np.random.shuffle(idxs)
-            T, T_, Y = [], [], []
-            for c, i in enumerate(idxs):
-                achievements = X1[i]
-                requirements = X2[i]
-                t, t_ = tokenizer.encode(first=achievements, second=requirements, max_len=MAX_LEN)
-                T.append(t)
-                T_.append(t_)
-                Y.append(y[i])
-                if len(T) == self.batch_size or i == idxs[-1]:
-                    T = np.array(T)
-                    T_ = np.array(T_)
-                    Y = np.array(Y)
-                    yield [T, T_], Y
-                    T, T_, Y = [], [], []
-
-
-def apply_multiple(input_, layers):
-    if not len(layers) > 1:
-        raise ValueError('Layers list should contain more than 1 layer')
-    else:
-        agg_ = []
-        for layer in layers:
-            agg_.append(layer(input_))
-        out_ = Concatenate()(agg_)
-    return out_
-
-
-def get_model():
-    bert_model = load_trained_model_from_checkpoint(config_path, checkpoint_path)
-    for l in bert_model.layers:
-        l.trainable = True
-
-    T1 = Input(shape=(None,))
-    T2 = Input(shape=(None,))
-
-    T = bert_model([T1, T2])
-
-    T = Lambda(lambda x: x[:, 0])(T)
-    T = Dense(30, activation='selu')(T)
-    T = BatchNormalization()(T)
-    output = Dense(2, activation='softmax')(T)
-    model = Model([T1, T2], output)
-    model.compile(
-        loss='categorical_crossentropy',
-        optimizer=Adam(1e-5),  # 用足够小的学习率
-        metrics=['accuracy']
-    )
-    model.summary()
-    return model
-
-
-class Evaluate(Callback):
-    def __init__(self, val_data, val_index):
-        self.score = []
-        self.best = 0.
-        self.early_stopping = 0
-        self.val_data = val_data
-        self.val_index = val_index
-        self.predict = []
-        self.lr = 0
-        self.passed = 0
-
-    def on_batch_begin(self, batch, logs=None):
-        """第一个epoch用来warmup，第二个epoch把学习率降到最低
-        """
-        if self.passed < self.params['steps']:
-            self.lr = (self.passed + 1.) / self.params['steps'] * learning_rate
-            K.set_value(self.model.optimizer.lr, self.lr)
-            self.passed += 1
-        elif self.params['steps'] <= self.passed < self.params['steps'] * 2:
-            self.lr = (2 - (self.passed + 1.) / self.params['steps']) * (learning_rate - min_learning_rate)
-            self.lr += min_learning_rate
-            K.set_value(self.model.optimizer.lr, self.lr)
-            self.passed += 1
-
-    def on_epoch_end(self, epoch, logs=None):
-        score, acc, f1 = self.evaluate()
-        if score > self.best:
-            self.best = score
-            self.early_stopping = 0
-            model.save_weights('bert{}.w'.format(fold))
+    @staticmethod
+    def label_process(x):
+        if x == 0:
+            return [1, 0]
         else:
-            self.early_stopping += 1
+            return [0, 1]
 
-    def evaluate(self):
-        self.predict = []
-        prob = []
-        val_x1, val_x2, val_y, val_cat = self.val_data
-        for i in tqdm(range(len(val_x1))):
-            achievements = val_x1[i]
-            requirements = val_x2[i]
+    def main(self):
+        tokenizer = Util._get_tokenizer(path=self.bert_vocab_path)
 
-            t1, t1_ = tokenizer.encode(first=achievements, second=requirements)
-            T1, T1_ = np.array([t1]), np.array([t1_])
-            _prob = model.predict([T1, T1_])
-            oof_train[self.val_index[i]] = _prob[0]
-            self.predict.append(np.argmax(_prob, axis=1)[0] + 1)
-            prob.append(_prob[0])
+        train = pd.read_csv(self.train_set_path)
+        test = pd.read_csv(self.dev_set_path, sep='\t')
+        train_achievements = train['question1'].values
+        train_requirements = train['question2'].values
+        labels = train['label'].values
 
-        score = 1.0 / (1 + mean_absolute_error(val_y + 1, self.predict))
-        acc = accuracy_score(val_y + 1, self.predict)
-        f1 = f1_score(val_y + 1, self.predict, average='macro')
-        return score, acc, f1
+        train['label'] = train['label'].apply(BertBase.label_process)
+        labels_cat = list(train['label'].values)
+        labels_cat = np.array(labels_cat)
+        test_achievements = test['question1'].values
+        test_requirements = test['question2'].values
+        print(train.shape, test.shape)
+
+        oof_train = np.zeros((len(train), 2), dtype=np.float32)
+        oof_test = np.zeros((len(test), 2), dtype=np.float32)
+
+        ind = np.array(list(range(train.shape[0])))
+        np.random.shuffle(ind)
+        print(ind)
+
+        train_index, valid_index = ind[:int(len(ind) * 0.8)], ind[int(len(ind) * 0.8):]
+
+        x1 = train_achievements[train_index]
+        x2 = train_requirements[train_index]
+        y = labels_cat[train_index]
+        val_x1 = train_achievements[valid_index]
+        val_x2 = train_requirements[valid_index]
+        val_y = labels[valid_index]
+        val_cat = labels_cat[valid_index]
+        train_D = DataGenerator(tokenizer=tokenizer, data=[x1, x2, y], batch_size=16, MAX_LEN=self.MAX_LEN)
+        model = Util.get_model(bert_config_path=self.bert_config_path, bert_checkpoint_path=self.bert_checkpoint_path)
+        evaluator = Evaluate(model=model, val_data=[val_x1, val_x2, val_y, val_cat], val_index=valid_index,
+                             learning_rate=self.learning_rate, min_learning_rate=self.min_learning_rate,
+                             tokenizer=tokenizer, oof_train=oof_train, fold=None)
+        model.fit_generator(train_D.__iter__(),
+                            steps_per_epoch=len(train_D),
+                            epochs=5,
+                            callbacks=[evaluator]
+                            )
+        model.load_weights('bert.w')
+        test = pd.DataFrame(Util.predict(data=[test_achievements, test_requirements], tokenizer=tokenizer, model=model))
+        K.clear_session()
+
+        test.to_csv('test_pred.csv', index=False)
+        test.head(), test.shape
+        train = pd.DataFrame(evaluator.get_oof_train())
+        train.to_csv('train_pred.csv', index=False)
+
+        pred = pd.read_csv('test_pred.csv').values
+        pred = pred.argmax(axis=1)
+        sub = pd.DataFrame()
+        sub['pred'] = list(pred)
+        sub.to_csv('sub.csv', sep='\t', header=None)
 
 
-def predict(data):
-    prob = []
-    val_x1, val_x2 = data
-    for i in tqdm(range(len(val_x1))):
-        achievements = val_x1[i]
-        requirements = val_x2[i]
-
-        t1, t1_ = tokenizer.encode(first=achievements, second=requirements)
-        T1, T1_ = np.array([t1]), np.array([t1_])
-        _prob = model.predict([T1, T1_])
-        prob.append(_prob[0])
-    return prob
-
+if __name__ == '__main__':
+    BertBase().main()
 
 # nfolds = 10
 # skf = StratifiedKFold(n_splits=nfolds, shuffle=True, random_state=42)
@@ -212,42 +123,3 @@ def predict(data):
 #     oof_test += predict([test_achievements, test_requirements])
 #     K.clear_session()
 # oof_test /= nfolds
-
-oof_train = np.zeros((len(train), 2), dtype=np.float32)
-oof_test = np.zeros((len(test), 2), dtype=np.float32)
-
-ind = np.array(list(range(train.shape[0])))
-np.random.shuffle(ind)
-print(ind)
-
-train_index, valid_index = ind[:int(len(ind) * 0.8)], ind[int(len(ind) * 0.8):]
-
-x1 = train_achievements[train_index]
-x2 = train_requirements[train_index]
-y = labels_cat[train_index]
-val_x1 = train_achievements[valid_index]
-val_x2 = train_requirements[valid_index]
-val_y = labels[valid_index]
-val_cat = labels_cat[valid_index]
-train_D = data_generator([x1, x2, y])
-evaluator = Evaluate([val_x1, val_x2, val_y, val_cat], valid_index)
-model = get_model()
-model.fit_generator(train_D.__iter__(),
-                    steps_per_epoch=len(train_D),
-                    epochs=7,
-                    callbacks=[evaluator]
-                    )
-model.load_weights('bert.w')
-test = pd.DataFrame(predict([test_achievements, test_requirements]))
-K.clear_session()
-
-test.to_csv('test_pred.csv', index=False)
-test.head(), test.shape
-train = pd.DataFrame(oof_train)
-train.to_csv('train_pred.csv', index=False)
-
-pred = pd.read_csv('test_pred.csv').values
-pred = pred.argmax(axis=1)
-sub = pd.DataFrame()
-sub['pred'] = list(pred)
-sub.to_csv('sub.csv', sep='\t', header=None)
--- demos/models/pytorch/bert_base.py
+++ demos/models/pytorch/bert_base.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
+from configurations.constant import Constant
+
+TYPE = 'bert'
+
+
+class BertBase(object):
+    def __init__(self):
+        self.configuration = Constant(type=TYPE).get_configuration()
+        self.project_path = Constant(type=TYPE).get_project_path()
+
+        self.bert_config_path = self.configuration.bert_config_path
+        self.bert_checkpoint_path = self.bert_checkpoint_path
+        self.bert_vocab_path = self.bert_vocab_path
