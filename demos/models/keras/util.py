48db99e88fc75f5e4f74cf463f321a21f81e0c00
wwwjjjnnn <1194348056@qq.com>
2019-11-19 21:17:04 +0800

    update bert_base. 线上 0.86920. 线下 0.9748.

--- demos/models/keras/util.py
+++ demos/models/keras/util.py
@@ -9,6 +9,67 @@ from keras.callbacks import Callback
 from sklearn.metrics import mean_absolute_error, accuracy_score, f1_score
 
 
+class Lookahead(object):
+    """Add the [Lookahead Optimizer](https://arxiv.org/abs/1907.08610) functionality for [keras](https://keras.io/).
+    """
+
+    def __init__(self, k=5, alpha=0.5):
+        self.k = k
+        self.alpha = alpha
+        self.count = 0
+
+    def inject(self, model):
+        """Inject the Lookahead algorithm for the given model.
+        The following code is modified from keras's _make_train_function method.
+        See: https://github.com/keras-team/keras/blob/master/keras/engine/training.py#L497
+        """
+        if not hasattr(model, 'train_function'):
+            raise RuntimeError('You must compile your model before using it.')
+
+        model._check_trainable_weights_consistency()
+
+        if model.train_function is None:
+            inputs = (model._feed_inputs +
+                      model._feed_targets +
+                      model._feed_sample_weights)
+            if model._uses_dynamic_learning_phase():
+                inputs += [K.learning_phase()]
+            fast_params = model._collected_trainable_weights
+
+            with K.name_scope('training'):
+                with K.name_scope(model.optimizer.__class__.__name__):
+                    training_updates = model.optimizer.get_updates(
+                        params=fast_params,
+                        loss=model.total_loss)
+                    slow_params = [K.variable(p) for p in fast_params]
+                fast_updates = (model.updates +
+                                training_updates +
+                                model.metrics_updates)
+
+                slow_updates, copy_updates = [], []
+                for p, q in zip(fast_params, slow_params):
+                    slow_updates.append(K.update(q, q + self.alpha * (p - q)))
+                    copy_updates.append(K.update(p, q))
+
+                # Gets loss and metrics. Updates weights at each call.
+                fast_train_function = K.function(
+                    inputs,
+                    [model.total_loss] + model.metrics_tensors,
+                    updates=fast_updates,
+                    name='fast_train_function',
+                    **model._function_kwargs)
+
+                def F(inputs):
+                    self.count += 1
+                    R = fast_train_function(inputs)
+                    if self.count % self.k == 0:
+                        K.batch_get_value(slow_updates)
+                        K.batch_get_value(copy_updates)
+                    return R
+
+                model.train_function = F
+
+
 class Util(object):
     """
     工具器
@@ -72,15 +133,13 @@ class Util(object):
         T = bert_model([T1, T2])
 
         T = Lambda(lambda x: x[:, 0])(T)
-        T = Dense(30, activation='selu')(T)
+        T = Dense(units=64, activation='selu')(T)
         T = BatchNormalization()(T)
         output = Dense(2, activation='softmax')(T)
         model = Model([T1, T2], output)
-        model.compile(loss='categorical_crossentropy',
-                      # 用足够小的学习率
-                      optimizer=Adam(1e-4),
-                      metrics=['accuracy']
-                      )
+        model.compile(loss='categorical_crossentropy', optimizer=Adam(1e-4), metrics=['accuracy'])
+        lookahead = Lookahead(k=5, alpha=0.5)  # 初始化Lookahead
+        lookahead.inject(model)  # 插入到模型中
         model.summary()
 
         return model
